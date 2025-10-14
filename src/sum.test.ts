15bbcfe3cb769a726e2f3614292812f8f2056f68
artmorai77 <arturmorais.ald@gmail.com>
2025-10-14 19:22:21 -0300

    Agrupando Testes

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,7 +1,15 @@
-import { sum } from "./server"
+import { sum } from "./server";
 
-test("sum of 3 + 7 equals 10", () => {
- const result = sum(7, 3)
+describe("sum", () => {
+  test("sum of 3 + 7 equals 10", () => {
+    const result = sum(7, 3);
 
-  expect(result).toBe(10)
-})
+    expect(result).toBe(10);
+  });
+
+  test("sum of 2 + 4 equals 6", () => {
+    const result = sum(2, 4);
+
+    expect(result).toBe(6);
+  });
+});
