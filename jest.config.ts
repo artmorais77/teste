5cc812105282662943ea56e99d1dcbd27d8bd92b
artmorai77 <arturmorais.ald@gmail.com>
2025-10-14 19:13:20 -0300

    Escrevendo o Primeiro Teste

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "jest"
 const config: Config = {
   bail: true,
   preset: "ts-jest",
-  testEnvironment: "node,"
+  testEnvironment: "node"
 }
 
 export default config
--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,3 +1,7 @@
-test("example", () => {
-  console.log("teste OK!")
+import { sum } from "./server"
+
+test("sum of 3 + 7 equals 10", () => {
+ const result = sum(7, 3)
+
+  expect(result).toBe(10)
 })
