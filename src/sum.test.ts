44198bcb34dc542e85698a2484b8b6358f517997
artmorai77 <arturmorais.ald@gmail.com>
2025-10-14 19:28:08 -0300

    beforeAll

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,10 +1,15 @@
 import { sum } from "./server";
 
 describe("sum", () => {
+  let sumResult: number
+
+  beforeAll(() => {
+    sumResult = 10
+  })
   it("sum of 3 + 7 equals 10", () => {
     const result = sum(7, 3);
 
-    expect(result).toBe(10);
+    expect(result).toBe(sumResult);
   });
 
   test("sum of 2 + 4 equals 6", () => {
